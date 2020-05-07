df1a3d880226138e3a57d7941b3fd19081a65924
Acrom <zvez.dmtr@gmail.com>
2020-05-07 11:14:24 +1000

    Add arguments input and npm run script

--- index.js
+++ index.js
@@ -5,12 +5,17 @@ const Text = require('./Text')
 Az.Morph.init(main)
 
 function main() {
-	fs.readFile('./input.txt', {encoding: 'utf-8'}, function (err, data) {
+	const percent = parseInt(process.argv[2], 10) || 30
+	const inputFile = process.argv[3] || './input.txt'
+	const outputFile = process.argv[4] || './output.txt'
+
+	fs.readFile(inputFile, {encoding: 'utf-8'}, function (err, data) {
+		if (err) throw err
 		const text = new Text(data)
-		let abstract = text.abstract(50)
-		fs.writeFile('output.txt', abstract, function (err) {
+		let abstract = text.abstract(percent)
+		fs.writeFile(outputFile, abstract, function (err) {
 			if (err) throw err
-			console.log('Saved!')
+			console.log('Done!')
 		})
 	})
 }
